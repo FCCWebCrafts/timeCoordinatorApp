4031585aa5bfab7426b279471679e6497e8ffc3a
A. Drake <a.l.drake713@gmail.com>
2015-03-12 11:20:13 -0400

    starting to split off views and services, etc. in angular

--- public/app.js
+++ public/app.js
@@ -1,13 +1,44 @@
-var app = angular.module('timeApp', []);
+var app = angular.module('timeApp', ['ui.router']);
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.notes = [
-  	'note 1', 
-  	'note 2', 
-  	'note 3',
-  	'note 4'
-  ];
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+		.state('dashboard', {
+			url: '/dashboard',
+			templateUrl: 'views/dashboard.html',
+			controller: 'DashCtrl'
+		})
+		.state('singleMeeting', {
+			url: '/meeting',
+			templateUrl: 'views/singleMeeting.html',
+			controller: 'DashCtrl'
+		});	
+		$urlRouterProvider.otherwise('home');
+	}]);
+
+// provides a list of meetings to other angular stuff that might need it
+app.factory('meetings', [function(){
+	var o = {
+		meetings: []
+		// can i get data from the database here??
+	};
+	return o;
+
+}]);
+
+// Controller for the Dashboard view - User can see meetings, other information, etc.
+app.controller('DashCtrl', [
+'$scope', 'meetings',
+function($scope, meetings){
+  $scope.meetings = meetings.meetings;
+
+  $scope.addMeeting = function() {
+  	if (!$scope.title || $scope.title === '') {return;}
+  	$scope.meetings.push({title: $scope.title,  description: $scope.description, date : "test value for now.", admin : "test value for now."});
+  	$scope.title = '';
+  	$scope.description = '';
+  };
+
 }]);
